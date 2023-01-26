ded4080964b76da0e35174fe3badbec67d1256d9
khalkhalyanartur <123370201+khalkhalyanartur@users.noreply.github.com>
2023-01-26 16:42:24 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,13 +1,46 @@
-const arr = [   { name: "Milk", price: 20, amount: 15 }, 
-                { name: "Coffee", price: 30, amount: 17}, 
-                { name: "Tea", price: 10, amount: 14 }      ] 
+/*
+Имеется массив объектов вида [{ name: “Milk”, price: 20, amount: 15 }, …], хранящий список товаров с их ценой и остатком на складе. Используя оператор map заполните новый массив суммарной стоимостью каждого товара на складе (price * amount).
 
-const totalGoods = function (goodsArray) {
-    return goodsArray.map((item) => (
-        {
-        name: item.name,
-        total: item.price * item.amount
-        }))
+
+
+Input: [{ name: “Milk”, price: 20, amount: 15 }, 
+
+ { name: “Coffee”, price: 30, amount: 17 },
+
+ { name: “Tea”, price: 10, amount: 14 }]
+
+
+
+Output: [{ name: “Milk”, total: 300 }, 
+
+ { name: “Coffee”, total: 510 },
+
+ { name: “Tea”, total: 140 }]
+
+
+*/
+
+
+
+const products = [   
+  { 
+    name: "Milk",
+    price: 20, amount: 15 
+  }, { 
+    name: "Coffee",
+    price: 30, amount: 17
+  }, { 
+    name: "Tea",
+    price: 10,
+    amount: 14 
+  }      
+] 
+
+const countTotalGoods = (array) => {
+  return array.map((item) => ({
+    name: item.name,
+    total: item.price * item.amount
+  }))      
 }
 
-console.log(totalGoods(arr))
+console.log(countTotalGoods(products));
