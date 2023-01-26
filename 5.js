d0e139cf97e429e9aa7e15bda79108b563dbd462
khalkhalyanartur <123370201+khalkhalyanartur@users.noreply.github.com>
2023-01-26 16:46:26 +0300

    Update 5.js

--- 5.js
+++ 5.js
@@ -1,10 +1,32 @@
-const arr =  [{a: 5, b: 3, c: 4}, {a: 1, b: 3, c: 4}, {a: 4, b: 7, c: 4}, {a: 2, b: 2, c: 3}]
+/*
+Имеется массив объектов, представляющий собой массив треугольников, где поля объекта a, b и c - стороны треугольника. Создать новый массив, заполнив его объектами с единственным полем p - периметром соответствующего треугольника.
 
-const totalGoods = function (goodsArray) {
-    return arr.map((item) => (
-        {
-        p: item.a + item.b + item.c
-        }))
+Input: [{a: 5, b: 3, c: 4}, {a: 1, b: 3, c: 4}, {a: 4, b: 7, c: 4}, {a: 2, b: 2, c: 3}]
+
+Output: [{ p: 12 }, { p: 8 }, { p: 15 }, { p: 7 }]
+*/
+const triangles =  [
+  {
+    a: 5,
+    b: 3,
+    c: 4
+  }, {
+    a: 1,
+    b: 3,
+    c: 4
+  }, {
+    a: 4,
+    b: 7,
+    c: 4
+  }, {
+    a: 2,
+    b: 2,
+    c: 3
+  }
+]
+
+const countPermeterTriangle = (array) => {
+  return array.map((item) => ({ p: item.a + item.b + item.c }));
 }
 
-console.log(totalGoods(arr))
+console.log(countPermeterTriangle(triangles))
