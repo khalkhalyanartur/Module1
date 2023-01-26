d7634f2b36c6d3e161958a0cb2e353395755bec0
khalkhalyanartur <123370201+khalkhalyanartur@users.noreply.github.com>
2023-01-26 16:48:26 +0300

    Update 8.js

--- 8.js
+++ 8.js
@@ -1,11 +1,31 @@
-const users = [{ name: "Ivan", age: 24 }, { name: "Oleg", age: 16}, { name: "Igor", age: 24}];
+/*
+Имеется массив пользователей вида [{ name: “Ivan”, age: 24 }]. Вывести суммарный возраст всех пользователей.
 
-const totalAge = function (array) {
-    let total = 0
-    array.forEach(element => {
-        total = total + element.age;
-    });
-    return total;
+Input: [{ name: “Ivan”, age: 24 }, { name: “Oleg”, age: 16}, { name: “Igor”, age: 24}]
+
+Output: 64
+*/
+
+const users = [
+  { 
+    name: "Ivan",
+    age: 24
+  }, { 
+    name: "Oleg",
+    age: 16
+  }, {
+    name: "Igor",
+     age: 24
+  }
+];
+
+const sumAge = (array) => {
+  let total = 0
+
+  array.forEach(element => {
+    total = total + element.age;
+  });
+  return total;
 }
 
-console.log(totalAge(users));
+console.log(sumAge(users));
