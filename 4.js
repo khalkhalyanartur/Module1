56019c461a7ceeaebaa3e87bb6e4966356a2f9a7
khalkhalyanartur <123370201+khalkhalyanartur@users.noreply.github.com>
2023-01-26 16:45:53 +0300

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,23 +1,39 @@
-const makeFibonacci = function (n) {
-    const arr = [];
+/*
+Написать функцию, принимающую число N, и возвращающую массив длиной N, заполненный числами Фибоначчи. Числа Фибоначчи - элементы числовой последовательности, в которой первые два числа равны 0 и 1, а каждое последующее число равно сумме двух предыдущих чисел (пример, 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233).
 
-    if (n === 0) {
-        arr = [0];
-        return arr;
-    }
+Input: 7
 
-    if (n === 1) {
-        arr = [0, 1];
-        return arr;
-    }
+Output: [0, 1, 1, 2, 3, 5, 8]
+*/
+const makeFibonacci = (count) => {
+  const arr = [];
 
+  if (count === 0) {
+
+    return arr;
+  }
+
+  if (count === 1) {
+    arr[0] = 0;
+
+    return arr;
+  }  
+
+  if (count === 2) {
     arr[0] = 0;
     arr[1] = 1;
 
-    for (i = 2; i < n; i++) {
-        arr[i] = arr[i - 1] + arr[i - 2]
-    }
     return arr;
+  }
+
+  arr[0] = 0;
+  arr[1] = 1; //заносим если count > 2
+
+  for (i = 2; i < count; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2]
+  }
+
+  return arr;
 }
 
-console.log(makeFibonacci(7));
+console.log(makeFibonacci(4));
