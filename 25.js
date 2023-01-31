40938d409ce019862938991dfab46ba19c8e9068
khalkhalyanartur <123370201+khalkhalyanartur@users.noreply.github.com>
2023-01-31 10:17:22 +0300

    Update 25.js

--- 25.js
+++ 25.js
@@ -1,25 +1,19 @@
 /*
 Написать функцию, принимающую на вход строку, написанную в стиле snake_case и возвращающую эту же строку, но уже в стиле camelCase.
-
-
-
 Input: find_and_replace_element_of_array
-
-
-
 Output: findAndReplaceElementOfArray
 */
+
 const string = 'find_and_replace_element_of_array';
 
 const convertSnakeToCamal = () => {
-
-const array = string.split('');
+  const array = string.split('');
   let newString;
   const result = [];
 
-  array.forEach((element, i, arr) => {
-    if (element != '_' && arr[i-1] !='_' ) result.push(element)
-    if (arr[i-1] === '_') result.push(element.toUpperCase());
+  array.forEach((element, i) => {
+    if (element != '_' && array[i-1] != '_' ) result.push(element)
+    if (array[i-1] === '_') result.push(element.toUpperCase());
   });
 
   return newString = result.join('');
