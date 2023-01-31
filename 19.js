c159685eaa0688bbc4734ae607b831c40f7372a9
khalkhalyanartur <123370201+khalkhalyanartur@users.noreply.github.com>
2023-01-31 10:13:49 +0300

    Update 19.js

--- 19.js
+++ 19.js
@@ -1,27 +1,21 @@
 /*
 Написать функцию, которая принимает на вход E-mal в виде строки и возвращает объект вида { username, domain }
-
-
-
 Input: “ivanov.oleg@pochta.com”
-
-
-
 Output: {
-
  username: “ivanov.oleg”,
-
  domain: “pochta.com”
-
 }
 */
 
 const addEmailToObj = (email) => {
   email = email + '';
+  const splitEmail = email.split('@');
+  
   return ({
-    username: email.split('@')[0],
-    domain: email.split('@')[1],
+    username: splitEmail[0],
+    domain: splitEmail[1],
   });
 }
 
 console.log(addEmailToObj("ivanov.oleg@pochta.com"));
+
