525dbe4288f5946716e059ac87ca297e1f24e448
khalkhalyanartur <123370201+khalkhalyanartur@users.noreply.github.com>
2023-01-31 10:16:30 +0300

    Update 23.js

--- 23.js
+++ 23.js
@@ -1,52 +1,37 @@
 /*
-
 Имеется массив строк. Строки в массиве могут повторяться любое количество раз. Вывести уникальные строки и их количество в массиве.
-
-
-
 Input: ['aaa', 'bbb', 'ccc', 'aaa', 'bbb', 'aaa']
-
-
-
 Output: 
-
 aaa 3
-
 bbb 2
-
 ccc 1
-
 */
 let arr = ['aaa', 'bbb', 'ccc', 'aaa', 'bbb', 'aaa'];
 
 const countUniqueStringInArray = (array) => {
+  const unique =[[],[]];
 
-const unique =[[],[]];
-
-unique[0] = array.filter((item, i, ar) => {
+  unique[0] = array.filter((item, i, ar) => {
   return ar.indexOf(item) === i
-});
+  });
+  unique[0].forEach(elementUnique => {
+    let countUniqe =  0;
+    array.forEach (element => {
+      if (element === elementUnique) countUniqe++; 
+    })
+    unique[1].push(countUniqe);
+  });
 
-unique[0].forEach(elementUnique => {
-  let countUniqe =  0;
-  array.forEach (element => {
-    if (element === elementUnique) countUniqe++; 
-  })
-  unique[1].push(countUniqe);
-});
-
-return unique;
-
+  return unique;
 }
 
-const prtArrayTwoDimensional = (array) => {
+const arrayToObj = (array) => {
+  const objUnique = {};
   for (let i = 0; i < array[0].length; i++ ) {
-    console.log(array[0][i],array[1][i]);
+    objUnique[array[0][i]] = array[1][i];
   }
+
+  return objUnique;
 }
 
-
-prtArrayTwoDimensional(countUniqueStringInArray(arr));
-
-
-
+console.log(arrayToObj(countUniqueStringInArray(arr)));
