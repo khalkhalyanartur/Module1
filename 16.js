26dff537afe3d3eacc514e60fd1acb663fe9f408
khalkhalyanartur <123370201+khalkhalyanartur@users.noreply.github.com>
2023-01-31 10:11:32 +0300

    Update 16.js

--- 16.js
+++ 16.js
@@ -6,7 +6,17 @@ Output: 8 (800) 555-35-35
 
 const formattingTelNumber = (telNumber) => {
   telNumber = telNumber + '';
-  return telNumber[0] + ' ' + '(' + telNumber.slice(1,4) + ')' + telNumber.slice(4,7) + '-' + telNumber.slice(7,9) + '-' + telNumber.slice(9,11); 
+
+  return telNumber[0] +
+  ' ' +
+  '(' +
+  telNumber.slice(1,4) + ')' +
+  ' ' +
+  telNumber.slice(4,7) +
+  '-' +
+  telNumber.slice(7,9) +
+  '-' +
+  telNumber.slice(9,11); 
 } 
 
-console.log(formattingTelNumber(88005550535));
+console.log(formattingTelNumber(88005553535));
