e5d4377dcbbe5d134e6ec94d47686a3bf24d4139
khalkhalyanartur <123370201+khalkhalyanartur@users.noreply.github.com>
2023-01-31 10:18:59 +0300

    Update 27.js

--- 27.js
+++ 27.js
@@ -6,8 +6,10 @@ Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 const concatArray = (array) => {
   let result = [];
-  for (let i = 0; i< array.length; i++) result = result.concat(array[i]);
-
+  for (let i = 0; i< array.length; i++) {
+    result = result.concat(array[i]);
+  }
+  
   return result;
 }
 
