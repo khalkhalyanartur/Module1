0ebd6804699b589964e34c6f745c8ec655dca692
khalkhalyanartur <123370201+khalkhalyanartur@users.noreply.github.com>
2023-01-31 10:15:40 +0300

    Update 22.js

--- 22.js
+++ 22.js
@@ -1,20 +1,21 @@
 /*
-Написать функцию, которая принимает три параметра: массив array и два числа start и end. Функция должна возвращать массив фрагмент массива array, начиная с индекса start и заканчивая end.
-
-
-
+Написать функцию, которая принимает три параметра: массив array и два числа start и end. 
+Функция должна возвращать массив фрагмент массива array, начиная с индекса start и заканчивая end.
 Input: [13, 14, 32, 23, 34, 21, 44, 47, 86], 4, 6
-
-
-
 Output: [34, 21, 44]
 */
 
 const returnPartArray = (array, start, end) => {
   const result = [];
+  
+  if (start < 0 || end < 0 || end > start) {
+
+    return result;
+  }
   for (let i = start; i <= end; i++) {
     result.push(array[i]);
   }
+  
   return result;
 }
 
