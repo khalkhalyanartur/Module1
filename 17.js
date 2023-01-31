e8317cee48476e99f2579a4d0baa282e43bf93b2
khalkhalyanartur <123370201+khalkhalyanartur@users.noreply.github.com>
2023-01-31 10:12:12 +0300

    Update 17.js

--- 17.js
+++ 17.js
@@ -2,15 +2,15 @@
 В функцию подается несколько массивов. Вернуть один массив со всеми элементами.
 */
 
-function concatArray() {
-  
+concatArray = (...arguments) => {
   const result = [];
+  
   for (let i = 0; i < arguments.length; i++) {
     for (let j = 0; j < arguments[i].length; j++) {
-      result.push(arguments[i][j])
+      result.push(arguments[i][j]);
     }
   }
-  return result  
+  return result;  
 }
 
 console.log(concatArray([1, 2],[3, 4],[5, 6]));
